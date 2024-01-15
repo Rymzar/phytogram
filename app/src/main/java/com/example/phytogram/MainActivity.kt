9dadd55c5fba9799c6bd5edca7dcbc0b0f1df408
CenitelNezhnihKuro4ek <rslnvaliev2@mail.ru>
2024-01-16 01:16:59 +0300

    так оно не крашится, меню навигейшн бар вызывает проблему(его просто закомитил пока),проблему еще не решил

--- app/src/main/java/com/example/phytogram/MainActivity.kt
+++ app/src/main/java/com/example/phytogram/MainActivity.kt
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
 import com.example.phytogram.R.id
-import com.example.phytogram.fragments.HomeActivity
+import com.example.phytogram.ui.HomeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val button = findViewById<TextView>(id.button)
+        button.setOnClickListener(){
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
 
 
     }
--- app/src/main/java/com/example/phytogram/ui/HomeActivity.kt
+++ app/src/main/java/com/example/phytogram/ui/HomeActivity.kt
@@ -1,4 +1,4 @@
-package com.example.phytogram.fragments
+package com.example.phytogram.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
