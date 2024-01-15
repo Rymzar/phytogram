2130eab9f06ec0b3f6cb7f40b53be006542ef331
CenitelNezhnihKuro4ek <rslnvaliev2@mail.ru>
2024-01-15 17:33:44 +0300

    add home_page:bottom_navigation, center_viewpager,top_tabs

--- app/src/main/java/com/example/phytogram/MainActivity.kt
+++ app/src/main/java/com/example/phytogram/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.phytogram
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.phytogram.R.id
+import com.example.phytogram.fragments.HomeActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
