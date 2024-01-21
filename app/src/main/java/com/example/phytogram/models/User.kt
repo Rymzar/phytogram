45fd91621ea8167757e0854cea28b4b118014f92
Nikta Zemlianskikh <rymzar@gmail.com>
2024-01-21 15:53:07 +0300

    refactor code

--- app/src/main/java/com/example/phytogram/models/User.kt
+++ app/src/main/java/com/example/phytogram/models/User.kt
@@ -1,12 +1,8 @@
 package com.example.phytogram.models
 
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
 // объект класса пользователя дл переда данных в БД
-@Entity(tableName = "user_data")
+
 class User(
-    @PrimaryKey
     val id: Int,
     val login: String,
     val email: String,
