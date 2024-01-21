e92bbbec84cc8452eba996bf7e916a1126910451
CenitelNezhnihKuro4ek <rslnvaliev2@mail.ru>
2024-01-21 14:08:50 +0300

    при нажатии на логин, выходит только подсказка... переход на хоум пейдж не осуществляется... мейн не трогаю, посмотри сам

--- app/src/main/java/com/example/phytogram/fragments/HomeActivity.kt
+++ app/src/main/java/com/example/phytogram/fragments/HomeActivity.kt
@@ -1,12 +1,36 @@
 package com.example.phytogram.fragments
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.phytogram.R
+import com.example.phytogram.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_home)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_add, R.id.navigation_notifications, R.id.navigation_guide
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/example/phytogram/fragments/add/AddViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.phytogram.fragments.add
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class AddViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is add Fragment"
+    }
+    val text: LiveData<String> = _text
+}
