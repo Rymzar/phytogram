fc2d8fad0431a030eb0ab538296ab7b7da77696e
CenitelNezhnihKuro4ek <rslnvaliev2@mail.ru>
2024-01-28 20:37:39 +0300

    переходит с логин на хоум

--- app/src/main/java/com/example/phytogram/fragments/HomeActivity.kt
+++ app/src/main/java/com/example/phytogram/fragments/HomeActivity.kt
@@ -22,12 +22,15 @@ class HomeActivity : AppCompatActivity() {
 
         val navView: BottomNavigationView = binding.navView
 
+
         val navController = findNavController(R.id.nav_host_fragment_activity_home)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_search, R.id.navigation_add, R.id.navigation_notifications, R.id.navigation_guide
+                R.id.navigation_home, R.id.navigation_search,
+                R.id.navigation_add, R.id.navigation_notifications,
+                R.id.navigation_guide
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
